7b5b9c26da0fe4c761a041ee295061924078fe98
guilherme <guilgchaves@protonmail.com>
2022-07-01 15:11:37 -0300

    Single Page Applications Part 1

--- week_2/src/components/MainComponent.js
+++ week_2/src/components/MainComponent.js
@@ -3,9 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
@@ -13,17 +17,23 @@ class Main extends Component {
 
     this.state = {
       dishes: DISHES,
-      //selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
-  // onDishSelect(dishId) {
-  //   this.setState({ selectedDish: dishId });
-  // }
-
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={
+            this.state.promotions.filter(promotion => promotion.featured)[0]
+          }
+          leader={this.state.leaders.filter(leader => leader.featured)[0]}
+        />
+      );
     };
 
     return (
