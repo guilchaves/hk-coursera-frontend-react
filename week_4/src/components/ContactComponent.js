a16343ecbeb75140c31bd871cd8aeaba4397b429
guilherme <guilgchaves@protonmail.com>
2022-07-04 15:36:02 -0300

    Combining Reducers

--- week_4/src/components/ContactComponent.js
+++ week_4/src/components/ContactComponent.js
@@ -1,12 +1,5 @@
 import React, { Component } from 'react';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  Label,
-  Col,
-  Row,
-} from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
@@ -60,8 +53,7 @@ class Contact extends Component {
               <br />
               <i className="fa fa-fax"></i>: +852 8765 4321
               <br />
-              <i className="fa fa-envelope"></i>:{' '}
-              <a href="mailto:confusion@food.net">confusion@food.net</a>
+              <i className="fa fa-envelope"></i>: <a href="mailto:confusion@food.net">confusion@food.net</a>
             </address>
           </div>
           <div className="col-12 col-sm-6 offset-sm-1">
@@ -69,25 +61,13 @@ class Contact extends Component {
           </div>
           <div className="col-12 col-sm-11 offset-sm-1">
             <div className="btn-group" role="group">
-              <a
-                role="button"
-                className="btn btn-primary"
-                href="tel:+85212345678"
-              >
+              <a role="button" className="btn btn-primary" href="tel:+85212345678">
                 <i className="fa fa-phone"></i> Call
               </a>
-              <a
-                role="button"
-                className="btn btn-info"
-                href="confusion@food.net"
-              >
+              <a role="button" className="btn btn-info" href="confusion@food.net">
                 <i className="fa fa-skype"></i> Skype
               </a>
-              <a
-                role="button"
-                className="btn btn-success"
-                href="mailto:confusion@food.net"
-              >
+              <a role="button" className="btn btn-success" href="mailto:confusion@food.net">
                 <i className="fa fa-envelope-o"></i> Email
               </a>
             </div>
@@ -220,21 +200,13 @@ class Contact extends Component {
                 <Col md={{ size: 6, offset: 2 }}>
                   <div className="form-check">
                     <Label check>
-                      <Control.checkbox
-                        model=".agree"
-                        name="agree"
-                        className="form-check-input"
-                      />{' '}
+                      <Control.checkbox model=".agree" name="agree" className="form-check-input" />{' '}
                       <strong>May we contact you?</strong>
                     </Label>
                   </div>
                 </Col>
                 <Col md={{ size: 3, offset: 1 }}>
-                  <Control.select
-                    model=".contactType"
-                    name="contactType"
-                    className="form-control"
-                  >
+                  <Control.select model=".contactType" name="contactType" className="form-control">
                     <option>Tel.</option>
                     <option>Email</option>
                   </Control.select>
